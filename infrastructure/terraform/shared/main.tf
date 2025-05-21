267105226d0254eba03237a8df8092193419795c
CabaVS <valerii.savchenko@icloud.com>
2025-05-21 17:44:55 +0200

    Change sampling percentage on App Insights

--- infrastructure/terraform/shared/main.tf
+++ infrastructure/terraform/shared/main.tf
@@ -14,7 +14,7 @@ resource "azurerm_application_insights" "app_insights" {
   resource_group_name  = var.resource_group_name
   application_type     = "web"
   workspace_id         = azurerm_log_analytics_workspace.law.id
-  sampling_percentage  = 10
+  sampling_percentage  = 100
   daily_data_cap_in_gb = 1
 }
 
