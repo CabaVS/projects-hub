97f70fa7b83dc3dfe77213be41600ede0e8b621d
CabaVS <valerii.savchenko@icloud.com>
2025-06-01 12:38:09 +0200

    Attempt to bootstrap an admin user for Keycloak on demand

--- projects/keycloak/bootstrap-and-start.sh
+++ projects/keycloak/bootstrap-and-start.sh
@@ -3,13 +3,9 @@ set -e
 
 # Conditionally create admin user
 if [[ -n "$KC_BOOTSTRAP_ADMIN_USERNAME" && -n "$KC_BOOTSTRAP_ADMIN_PASSWORD" ]]; then
-  echo "[CVS_BOOT] Bootstrapping admin user..."
-  /opt/keycloak/bin/kc.sh bootstrap-admin \
-    --user "$KC_BOOTSTRAP_ADMIN_USERNAME" \
-    --password "$KC_BOOTSTRAP_ADMIN_PASSWORD"
+  exec /opt/keycloak/bin/kc.sh start --optimized --http-port=8080 \
+    --bootstrap-admin-username="${KC_BOOTSTRAP_ADMIN_USERNAME}" \
+    --bootstrap-admin-password="${KC_BOOTSTRAP_ADMIN_PASSWORD}"
 else
-  echo "[CVS_BOOT] Admin user creation skipped."
+  exec /opt/keycloak/bin/kc.sh start --optimized --http-port=8080
 fi
-
-# Start Keycloak
-exec /opt/keycloak/bin/kc.sh start --optimized --http-port=8080
